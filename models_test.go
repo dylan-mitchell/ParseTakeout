d4a80710de06cbbe97bc619337ea0e55fccf2d87
dylan-mitchell <djmitc04@louisville.edu>
2020-02-08 17:45:51 -0500

    Leave results url encoded

--- models_test.go
+++ models_test.go
@@ -118,7 +118,7 @@ func TestGetYears(t *testing.T) {
 }
 
 func TestSearchItems(t *testing.T) {
-	searchString := "google"
+	searchString := "forestgiant"
 	db, err := OpenDB(testHome + "takeout.db")
 	if err != nil {
 		t.Fatal(err)
@@ -128,6 +128,8 @@ func TestSearchItems(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	fmt.Println(results)
 
 	fmt.Println(fmt.Sprintf("Search for '%s' returned %d results", searchString, len(results)))
 }
